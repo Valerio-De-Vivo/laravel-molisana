94b5d86f19ce443e69b2cb3f2c44242482f607ad
Valerio <valeriodevivo90@gmail.com>
2021-03-11 16:55:30 +0100

    Up with route product

--- resources/views/prodotti.blade.php
+++ resources/views/prodotti.blade.php
@@ -6,11 +6,11 @@
 
 @section('content')
     <div class="shop">
-        @foreach ($formati as $prod)
+        @foreach ($formati as $k => $prod)
             <div class="prod-card">
                 <img src=" {{$prod['src']}} " alt="">
                 <div class="overlay">
-                    <h2> {{$prod['titolo']}} {{$prod['tipo']}} </h2>
+                    <a href=" {{ route('dettagli-prodotto', ['id' => $k ]) }} "><h2> {{$prod['titolo']}} {{$prod['tipo']}} </h2></a>
                 </div>
             </div>
         @endforeach
---
+++ resources/views/prodotto.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title')
+    La Molisana
+@endsection
+
+@section('content')
+    prodotto 
+@endsection
